df99d919f5ded0c875e44750bdda2beabe3d430a
Redwanul-Hassan-Labib <web.redwanlabib@gmail.com>
2025-11-10 12:31:16 +0600

    nonPrimitive Array

---
+++ src/nonPrimitiveArr.ts
@@ -0,0 +1,24 @@
+//TODO array or object defined
+
+//! Array defined
+let bazarlist: string[] = []
+
+bazarlist.push(" egges" , "milk")
+
+//console.log(bazarlist);
+
+
+//! mixed array number or string
+
+let mixedarr:(string | number)[] = ["agges",  12, "milk" , 1, "suger", 2]
+
+
+mixedarr.push("redbull",25)
+
+//console.log(mixedarr);
+
+let tuple: [string, number] = ["husband", 25]
+
+tuple[1]=30
+
+console.log(tuple);
